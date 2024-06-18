a4690b2284e5becc92d6719bd189d1091cc81ba0
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-18 16:07:38 +0900

    add : auth 리소스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios/dist/http.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
